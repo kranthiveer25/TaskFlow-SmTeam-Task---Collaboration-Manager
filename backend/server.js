b74e51222493e309d9e98e5aea234e890a25b888
kranthiveer <kranthiveer.kammari9@gmail.com>
2026-03-05 11:26:41 +0530

    Milestone 1 & 2: Project setup and MongoDB connection

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('TaskFlow API is running...');
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
